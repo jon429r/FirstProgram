8eaa33e901140b07562b7439d20fbcf951acae50
jon429r <jonathanday088@gmail.com>
2023-02-27 01:44:16 -0500

    Reads in from a file
    file named Student.txt used to read in input and create new student objects
    does have an issue at line 15

--- App.java
+++ App.java
@@ -1,15 +1,21 @@
-
+import java.util.Scanner;
+import java.util.ArrayList;
 
 public class App{
+
+    public static ArrayList<Student> dataBase = new ArrayList<>(); 
+
     public static void main(String args[]){
-        Student std1 = new Student();
-        std1.setfirstName("Elizabeth");
-        std1.setlastName("Day");
-        std1.setidNumber(111111);
-        std1.setyear(3);
 
-        System.out.println(std1);;
+        Scanner sc = new Scanner("Student.txt");
+
+        while(sc.hasNextLine()){
+            Student std1 = new Student();
+            std1.setfirstName(sc.nextLine());
+            std1.setlastName(sc.nextLine());
+            std1.setidNumber(Integer.parseInt(sc.nextLine()));
+            std1.setyear(Integer.parseInt(sc.nextLine()));
+            dataBase.add(std1);
+        }
     }
-
-
 }
